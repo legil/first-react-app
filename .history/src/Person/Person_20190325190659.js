b2cf7d2bdc7dac96f21d40d85080b46c0bf973c7
legil <leslieg033@gmail.com>
2019-03-25 19:10:39 -0400

    dynamic component using props

--- .history/src/Person/Person_20190325190659.js
+++ .history/src/Person/Person_20190325190659.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { prependOnceListener } from 'cluster';
 
 
 const person = (props) => {
     return (
         <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>I'm {props.name} and I am {props.age}} years old!</p>
             <p>{props.children}</p>
         </div>
     )}
