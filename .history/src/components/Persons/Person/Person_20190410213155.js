e59c3eff9c73825a6bac128f421dac8530ea834e
legil <leslieg033@gmail.com>
2019-04-10 21:38:16 -0400

    focus last input using refs

--- .history/src/components/Persons/Person/Person_20190410213155.js
+++ .history/src/components/Persons/Person/Person_20190410213155.js
@@ -9,14 +9,8 @@ import classes from './Person.css';
 
 
 class Person extends Component{
-    constructor(props){
-        super(props);
-        this.inputElementRef = React.createRef();
-    }
-
     componentDidMount(){
-        //this.inputElement.focus();
-        this.inputElementRef.current.focus();
+        this.inputElement.focus();
     }
 
     render() {
@@ -25,12 +19,7 @@ class Person extends Component{
         <Aux>
             <p key = "i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
             <p key = "i2">{this.props.children}</p>
-            <input key ="i3" 
-            type="text" 
-            // ref ={(inputEl) => {this.inputElement = inputEl}}
-            ref={this.inputElementRef}
-            onChange={this.props.changed} 
-            value = {this.props.name}/>
+            <input key ="i3" type="text" ref ={(inputEl) => {this.inputElement = inputEl}} onChange={this.props.changed} value = {this.props.name}/>
         </Aux>
         );
     }
