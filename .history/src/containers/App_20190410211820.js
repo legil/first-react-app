195db4a3f24dc57b48d8b0149707561ace934910
legil <leslieg033@gmail.com>
2019-04-10 21:19:00 -0400

    proptypes

---
+++ .history/src/containers/App_20190410211820.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Auxiliary';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
+  state = {
+    persons: [
+      { id: '1', name: 'Max', age: 26 },
+      { id: '2', name: 'Steph', age: 28 },
+      { id: '3', name : 'Jay', age: 23 }
+    ],
+    otherState: 'some other value',
+    showPersons: false,
+    showCockpit: true,
+    changeCounter: 0
+  }
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] getDerivedStateFromProps', props)
+    return state;
+  }
+
+  componentDidMount(){
+    console.log('[App.js] componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[App.js] shouldComponentUpdate');
+    return true; //default
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate');
+  }
+
+
+  //binding and two way binding example - on type
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    })
+    
+    const person = {...this.state.persons[personIndex]};
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState((prevState, props) => {
+      return{ 
+        persons: persons,
+        changeCounter: this.state.changeCounter + 1 
+      };
+    });
+  };
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice(); //makes copy of original array instead of referencing
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+
+  }
+
+ togglePersonsHandler = () => {
+      const doesShow = this.state.showPersons;
+      this.setState({showPersons : !doesShow})
+    }
+
+  render() {
+    console.log('[App.js] render');
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = ( <Persons 
+            persons = {this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed = {this.nameChangedHandler}
+          />
+
+      );
+      
+
+    }
+
+    return (
+      <Aux>
+        <button onClick = {() => {this.setState({showCockpit: false})}}>
+          Remove Cockpit
+        </button>
+        {this.state.showCockpit ? (
+        <Cockpit title = {this.props.appTitle}
+                showPersons={this.state.showPersons}
+                 personsLength = {this.state.persons.length}
+                 clicked = {this.togglePersonsHandler} />
+          ) : null} 
+          {persons}
+      </Aux>
+    );
+    //^ bind is better to avoid too much re rendering
+   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default withClass(App, classes.App);
